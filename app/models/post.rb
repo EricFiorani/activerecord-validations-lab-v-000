2062dfd801be22644b1fbb5c0d3bcacd98fc62d8
Eric Fiorani <efiorani305@gmail.com>
2018-11-13 15:12:08 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    if record.title
+      unless record.title.match /Secret|Top|Guess|Won't Believe/
+        record.errors[:title] << 'Not sufficiently click-bait-y'
+      end
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  
 end
