a239310342eeaab83abfeadc6cb5c279fd1ee1ef
Eric Fiorani <efiorani305@gmail.com>
2018-11-13 15:13:43 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,6 @@ end
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
+  validates :summart, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
