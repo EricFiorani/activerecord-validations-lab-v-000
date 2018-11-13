5a646f4ed417da38c2955bfe2e4bc0e1e43bdc97
Eric Fiorani <efiorani305@gmail.com>
2018-11-13 15:15:27 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,7 @@ end
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
-  validates :summart, length: { maximum: 250 }
+  validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   include ActiveModel::Validations
   validates_with Validator
